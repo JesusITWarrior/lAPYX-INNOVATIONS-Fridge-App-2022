31f094c77e4a41131d00da4027db02fd6a792b8b
JITWGaming <jesusftw99@gmail.com>
2022-02-11 15:55:12 -0600

    Add project files.

---
+++ lAPYX INNOVATIONS Fridge App 2022/lAPYX INNOVATIONS Fridge App 2022/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using lAPYX_INNOVATIONS_Fridge_App_2022.Services;
+using lAPYX_INNOVATIONS_Fridge_App_2022.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lAPYX_INNOVATIONS_Fridge_App_2022
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lAPYX INNOVATIONS Fridge App 2022/lAPYX INNOVATIONS Fridge App 2022/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using lAPYX_INNOVATIONS_Fridge_App_2022.ViewModels;
+using lAPYX_INNOVATIONS_Fridge_App_2022.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace lAPYX_INNOVATIONS_Fridge_App_2022
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
